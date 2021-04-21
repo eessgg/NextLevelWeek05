8a563f00b87678cd6c98a6e9bae6be009e8c8c58
eessgg <estergomesdesign@gmail.com>
2021-04-20 21:11:22 -0300

    aula 2 react done

--- podcaster-next/src/pages/index.tsx
+++ podcaster-next/src/pages/index.tsx
@@ -1,9 +1,22 @@
 
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes)
   return (
     <>
       <h1>HOME</h1>
     </>
   )
+}
+
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
 }
